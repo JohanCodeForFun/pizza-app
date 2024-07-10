0e08a7ece6447dab001093a3faeb9af41a534f18
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-10 18:21:33 +0200

    add get pizza detail function

--- src/index.ts
+++ src/index.ts
@@ -81,3 +81,18 @@ completeOrder(8)
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
 console.log("Order Qeue:", orderQueue)
+
+
+function getPizzaDetail(identifier: string | number) {
+  if (typeof identifier === "string") {
+    return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase())
+  } else if (typeof identifier === "number") {
+    return menu.find(pizza => pizza.id === identifier);
+  } else {
+    throw new TypeError("Parameter `identifier` must be either a string or a number")
+  }
+}
+
+console.log(getPizzaDetail(2))
+console.log(getPizzaDetail(3))
+console.log(getPizzaDetail(5))
